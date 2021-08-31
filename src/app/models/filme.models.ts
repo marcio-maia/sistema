5edd30579b1c0c2015fb8e31ead6adce30465494
marcio-maia <marcio_greg@hotmail.com>
2021-08-31 20:05:10 -0300

    Adicionando a interface dos filmes

---
+++ src/app/models/filme.models.ts
@@ -0,0 +1,8 @@
+export interface IFilme{
+  nome: string;
+  lancameto: string;
+  duracao: string;
+  classificacao: number;
+  cartaz: string;
+  generos: string[];
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/filme.models';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,29 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos App';
+  listaVideos: IFilme[] =  [
+    {
+      nome:'Lobo Guerreiro 2 (2017)',
+      lancameto: '27/07/2017',
+      duracao: '2h 4m',
+      classificacao: 76,
+      cartaz:'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/nJ8V8eOLWEKnZjvRZkyI8BRMKDf.jpg',
+      generos:['Guerra', 'Ação', 'Drama']
+
+    },
+    {
+      nome:'Pagando Bem, Que Mal Tem? (2008)',
+      lancameto:'18/09/2008',
+      duracao:'1h 42m',
+      classificacao:76 ,
+      cartaz:'https://www.themoviedb.org/t/p/w220_and_h330_face/2gL5KB4GV51O6rH9HTeDGeTy0XX.jpg',
+      generos:['Comedia']
+    }
+
+
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController ) {}
   async exibirAlertaFavorito() {
